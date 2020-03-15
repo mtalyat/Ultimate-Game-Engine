add58cb1139edd9365be7117f7a3bc5e96d349f6
mitchell.talyat <mitchell.talyat@gmail.com>
2020-03-15 18:17:24 -0500

    Added locks to Debug.

--- Engine/Scripts/Debug.cs
+++ Engine/Scripts/Debug.cs
@@ -3,6 +3,8 @@ using System.IO;
 
 namespace UltimateEngine{
 	public static class Debug{
+		private static object monitor = new object();
+
 		static string path = "";
 
 		static Debug(){
@@ -10,12 +12,18 @@ namespace UltimateEngine{
 		}
 
 		public static void Reset(){
-			//clear log with each new session
-			File.WriteAllText(path, $"LOG FOR {DateTime.Now}. Reminder: The log will reset each time the program is ran.\n");
+			lock (monitor)
+			{
+				//clear log with each new session
+				File.WriteAllText(path, $"LOG FOR {DateTime.Now}. Reminder: The log will reset each time the program is ran.\n");
+			}
 		}
 
 		public static void Log(object o){
-			File.AppendAllText(path, o.ToString() + '\n');
+			lock (monitor)
+			{
+				File.AppendAllText(path, o.ToString() + '\n');
+			}
 		}
 
 		public static void LogError(object o)
